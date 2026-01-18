3d7e1bd4b0861c2445602b4cc9ca7575be1346fd
HunterCSUTU <hunternelson74@gmail.com>
2026-01-17 17:29:59 -0700

    Added walking sprite and sprite change logic

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
     GamePanel gp;
@@ -26,12 +29,35 @@ At this point:
         this.gp = gp;
         this.keyH = keyH;
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
+    }
+
+    public void getPlayerImage() {
+        try {
+            /*
+            for the filepath  // Loads the image from the classpath starting at the root;
+            at compile time it’s copied to the output folder, so source folder names (res/src)
+            aren’t part of the path.
+            */
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void update() {
@@ -39,23 +65,74 @@ At this point:
 
         if (keyH.upPressed) {
             //System.out.println("key pressed working");
+            direction = "up";
             y -= speed;
         }
         else if (keyH.downPressed) {
+            direction = "down";
             y += speed;
         }
         else if (keyH.leftPressed) {
+            direction = "left";
             x -= speed;
         }
         else if (keyH.rightPressed) {
+            direction = "right";
             x += speed;
+        }
+
+        spriteCounter++;
+        if (spriteCounter > 10) {
+            if (spriteNum == 1) {spriteNum = 2;}
+            else if (spriteNum == 2) {spriteNum = 1;}
+            spriteCounter = 0;
         }
     }
     public void draw(Graphics2D g2) {
         //System.out.println("player draw running");
+        //g2.setColor(Color.white);
+        //g2.fillRect(x, y, gp.tileSize, gp.tileSize); //player rn
 
-        g2.setColor(Color.white);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize); //player rn
+        BufferedImage image = null;
+
+        switch (direction) {
+            case "up":
+                if (spriteNum == 1) {image = up1;}
+                if (spriteNum == 2) {image = up2;}
+                break;
+            case "down":
+                if (spriteNum == 1) {image = down1;}
+                if (spriteNum == 2) {image = down2;}
+                break;
+            case "left":
+                if (spriteNum == 1) {image = left1;}
+                if (spriteNum == 2) {image = left2;}
+                break;
+            case "right":
+                if (spriteNum == 1) {image = right1;}
+                if (spriteNum == 2) {image = right2;}
+                break;
+        }
+
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
